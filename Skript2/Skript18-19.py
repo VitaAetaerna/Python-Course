68ebb6436ee568028c10d6e95eda9e04322669d8
VitaAetaerna <muhleon18@gmail.com>
2024-04-10 09:55:53 +0200

    Added 18 - 19

---
+++ Skript2/Skript18-19.py
@@ -0,0 +1,14 @@
+# 18. Was versteht man unter dem Begriff ’Komplexitatstheorie?
+# Was genau versteht man unter dem generellen Begriff ’Komplexit¨at’?
+
+# Die Komplexitätstheorie als Teilgebiet der theoretischen Informatik befasst
+# sich mit der Komplexität algorithmisch behandelbarer Probleme auf verschiedenen formalen Rechnermodellen.
+# Komplexität beschreibt die Verwendung von Resourcen und die Schnelligkeit einer Instanz (Algorithmus)
+
+# 19. Beschreibe die Begriffe ’Zeitkomplexität’ und ’Speicherkomplexität.
+# Die Zeitkomplexität eines Algorithmus ist die benötigte Zeit, um den Algorithmus
+# als Funktion der Länge der Eingabe auszuführen.
+
+# Die Speicherkomplexität ist der minimaler Speicherplatzbedarf eines Algorithmus. Das Interessante 
+# ist nicht der Speicherbedarf eines Programms, sondern wie der Speicheraufwand bei mehreren 
+# Datenverarbeitungen wächst.
